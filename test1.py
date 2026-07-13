bea92f2d8d8353d9cebda5fee060fab9d4d6a922
Gz53841645 <3496831493@qq.com>
2026-07-13 11:11:36 +0800

    修改默认昵称为吴世勋

--- test1.py
+++ test1.py
@@ -105,7 +105,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 # 昵称
 if "nick_name" not in st.session_state:
-    st.session_state.nick_name = "卡卡西博文桑"
+    st.session_state.nick_name = "吴世勋"
 # 性格
 if "nature" not in st.session_state:
     st.session_state.nature = "现实里的的火影忍者卡卡西"
